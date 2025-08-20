c59f8cc295d4ef7fe1b8e06a4ce5681725b06ffb
Pratham Saha <prathamsaha15@gmail.com>
2025-08-20 13:15:21 +0530

    Add cookie-parser and cors middleware; implement ApiError and ApiResponse classes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import dotenv from 'dotenv';
 //import mongoose from "mongoose";
 //import {DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from './app.js';
+
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server is running at ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+    
+})
 
 
 
