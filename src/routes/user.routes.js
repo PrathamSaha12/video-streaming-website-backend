d7e6bb7d46d699da29fb8d98d2f2b0df1167c37d
Pratham Saha <prathamsaha15@gmail.com>
2025-08-30 14:06:55 +0530

    Refactor user routes; organize imports and add missing route handlers for change password, current user, update details, avatar, cover image, and user channel profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/User.controller.js";
+import { 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWtchHistroy, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAccountDetails, 
+    updateCoverImage, 
+    updateUserAvatar 
+} 
+from "../controllers/User.controller.js";
 import  {upload}  from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,6 +39,11 @@ router.route("/login").post(loginUser)
 //secure routes
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-details").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/coverimage").patch(verifyJWT,upload.single("coverimage"),updateCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/histroy").get(verifyJWT,getWtchHistroy)
 export default router
