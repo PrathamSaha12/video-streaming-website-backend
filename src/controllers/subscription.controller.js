1cc94d8f69861740d46df22405054758e595ec1e
Pratham Saha <prathamsaha15@gmail.com>
2025-09-08 21:12:08 +0530

    Refactor subscription routes and enhance toggleSubscription error handling; implement subscriber count retrieval and channel details fetching

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -10,7 +10,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
     const {channelId} = req.params
     // TODO: toggle subscription
-    if(!channelId){
+    if(!channelId || !isValidObjectId(channelId)){
         throw new ApiError(404,"channel Id not found")
     }
 
@@ -37,7 +37,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         if(!subscribe){
             throw new ApiError(400,"error occur while subscribing")
         }
-        subs:true
+        subs=true
     }
 
     //for unsbscribe
@@ -51,7 +51,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         if(!unsbscribe){
             throw new ApiError(400,"error occur while unsubcribing")
         }
-        subs:false
+        subs=false
     }
 
     return res
@@ -70,11 +70,101 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     if(!channelId){
         throw new ApiError(404,"channel id not found")
     }
+
+    //const user = req.user?._id
+
+    const allSubscribers = await Subscription.aggregate([
+        {
+            $match:{
+                channel : new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $group:{
+                _id: "$channel",
+                subscriberCount:{
+                    $sum:1
+                }
+            }
+        },
+        {
+            $project:{
+                channel:1,
+                subscriberCount:1
+            }
+        }
+    ])
+
+    if(!allSubscribers || allSubscribers.length===0){
+        throw new ApiError(400,"no subscriber yet")
+    }
+
+    return res
+    .status(200)
+    .json(
+       new ApiResponse (200,allSubscribers,"all subscribers fetched successfully")
+    )
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!subscriberId || !isValidObjectId(subscriberId)){
+        throw new ApiError(400,"invalid or missing subscriberId")
+    }
+
+    const userId = req.user?._id
+    if(!userId){
+        throw new ApiError(400,"invalid or missing userid")
+    }
+
+    const allChannel = await Subscription.aggregate([
+        {
+            $match:{
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"channel",
+                foreignField:"_id",
+                as:"channeldetails",
+                pipeline:[
+                    {
+                        $project:{
+                            username:1,
+                            fullname:1,
+                            avatar:1,
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                channeldetails:{
+                    $first:"$channeldetails"
+                }
+            }
+        },
+        {
+            $project:{
+                channeldetails:1
+            }
+        }
+    ])
+
+    if(!allChannel || allChannel.length === 0){
+        throw new ApiError(400,"user not subscribe any channel")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,allChannel,"all channel list fetched successfully")
+    )
 })
 
 export {
